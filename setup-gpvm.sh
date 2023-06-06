1d677bea90147cccb2c5f5b26b679ab42922699f
hyperfiner <rfine4@gmail.com>
2023-06-06 16:04:26 -0500

    Removing deprecated setup script

--- setup-gpvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export TOPDIR=/minerva/app/users/finer/MATAna/
-
-source ../opt/bin/setupROOT6OnGPVMs.sh
-source ../opt/bin/setup.sh
-source ../opt/buildGENIEXSecExtract/setup_GENIEXSecExtract.sh 
-
-export IFDH_DEBUG=0
