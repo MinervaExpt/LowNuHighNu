fe825eaf3bd85e779ef9e0ab1b1f6cdb27332563
hyperfiner <rfine4@gmail.com>
2023-06-06 16:58:05 -0500

    Remove deprecated method, add commenting, and prepare for file name change

--- ccpion_common.h
+++ ccpion_common.h
@@ -1,11 +1,13 @@
-#ifndef ccpion_common_h
-#define ccpion_common_h
+#ifndef playlist_methods_h
+#define playlist_methods_h
 
 #include <algorithm>  // transform
 #include <string>
 
 #include "TString.h"  // Form
 
+//RDF// This is eventually going to point to p4 MAD tuples and can be shared b/w
+//RDF// me and Ben/Ever
 std::string GetPlaylistFile(std::string plist, bool is_mc,
                             bool use_xrootd = true) {
   // const std::string processing_date = "20200713"; // new short tracking branches
@@ -28,22 +30,6 @@ std::string GetPlaylistFile(std::string plist, bool is_mc,
 std::string GetTestPlaylist(bool is_mc) {
   return is_mc ? "/minerva/app/users/bmesserl/MATAna/cc-ch-pip-ana/cache/ME1A_mc_p3_beta.txt" :
                  "/minerva/app/users/bmesserl/MATAna/cc-ch-pip-ana/cache/ME1A_data_p3_beta.txt";
-
 }
 
-std::string GetPlaylistFileCCPi(std::string plist, bool is_mc,
-                                bool use_xrootd = true) {
-  const std::string processing_date = "20210307";
-  const std::string is_mc_str = is_mc ? "mc" : "data";
-  std::transform(plist.begin(), plist.end(), plist.begin(), ::toupper);
-  std::string topdir =
-      "/minerva/data/users/bmesserl/MECCCHpip_ana_plists/" + processing_date;
-  std::string playlist_file =
-      use_xrootd ? Form("%s/%s_%s_xrootd_plist.txt", topdir.c_str(),
-                        is_mc_str.c_str(), plist.c_str())
-                 : Form("%s/%s_%s_plist.txt", topdir.c_str(), is_mc_str.c_str(),
-                        plist.c_str());
-  return playlist_file;
-}
-
-#endif  // ccpion_common_h
+#endif  // playlist_methods_h
