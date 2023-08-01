7eeb791d0a224f653874fb5a8733ba5131a8469a
hyperfiner <rfine4@gmail.com>
2023-08-01 14:55:24 -0500

    Branching of CCPiEvent class and ccpi_event namespace into LowNuHighNuEvent class and lownuhighnu_event namespace, intentionally leaving behind cookie-crumb-esque trail for Anne to follow

---
+++ includes/LowNuHighNuEvent.h
@@ -0,0 +1,84 @@
+#ifndef LowNuHighNuEvent_h
+#define LowNuHighNuEvent_h
+
+#include "CVUniverse.h"
+#include "Constants.h"  // typedef RecoPionIdx, EventCount, PassesCutsInfo
+#include "SignalDefinition.h"
+#include "TruthCategories/Sidebands.h"         // WSidebandType
+#include "TruthCategories/SignalBackground.h"  // SignalBackgroundType
+#include "Variable.h"
+class Variable;
+
+//==============================================================================
+// LowNuHighNuEvent is a container struct holding misc info about a universe-event:
+// * passes cuts
+// * is signal
+// * event weight
+// * is mc, is truth
+// * vector of candidate pion indices
+// * signal definition currently being used
+// * whether and what kind of w sideband it is
+// And it owns a pointer to the corresponding CVUniverse.
+//
+// This container exists to be passed around to functions that need all this
+// info: functions that fill histograms, check whether the event-universe
+// passed cuts, etc.
+//
+// LowNuHighNuEvent has no functions of its own.
+//==============================================================================
+
+struct LowNuHighNuEvent {
+  LowNuHighNuEvent(const bool is_mc, const bool is_truth,
+            const SignalDefinition signal_definition, CVUniverse* universe);
+
+  // Fixed by the constructor
+  const bool m_is_mc;
+  const bool m_is_truth;
+  const SignalDefinition m_signal_definition;
+  CVUniverse* m_universe;
+  std::vector<RecoPionIdx>
+      m_reco_pion_candidate_idxs;  // initialized empty, filled by PassesCuts
+  bool m_is_signal;
+  double m_weight;
+  WSidebandType m_w_type;
+
+  // Fixed (directly) outside of constructor -- with time-intensive functions
+  bool m_passes_cuts;
+  bool m_is_w_sideband;
+  bool m_passes_all_cuts_except_w;
+  RecoPionIdx m_highest_energy_pion_idx;  // GetHighestEnergyPionCandidateIndex
+};
+
+// Helper Functions
+// bool IsWSideband(LowNuHighNuEvent&);
+PassesCutsInfo PassesCuts(const LowNuHighNuEvent&);
+RecoPionIdx GetHighestEnergyPionCandidateIndex(const LowNuHighNuEvent&);
+SignalBackgroundType GetSignalBackgroundType(const LowNuHighNuEvent&);
+
+// Helper Fill Histo Functions
+namespace lownuhighnuevent {
+// Xsec analysis fill functions
+void FillSelected(const LowNuHighNuEvent&, const std::vector<Variable*>&);
+void FillRecoEvent(const LowNuHighNuEvent&, const std::vector<Variable*>&);
+void FillWSideband(const LowNuHighNuEvent&, const std::vector<Variable*>&);
+void FillTruthEvent(const LowNuHighNuEvent&, const std::vector<Variable*>&);
+void FillEfficiencyDenominator(const LowNuHighNuEvent&, const std::vector<Variable*>&);
+void FillMigration(const LowNuHighNuEvent&, const std::vector<Variable*>&,
+                   std::string name);
+
+// Study functions
+void FillWSideband_Study(const LowNuHighNuEvent&, std::vector<Variable*>);
+void FillCounters(const LowNuHighNuEvent&,
+                  const std::pair<EventCount*, EventCount*>& counters);
+std::pair<EventCount, EventCount> FillCounters(const LowNuHighNuEvent&,
+                                               const EventCount& signal,
+                                               const EventCount& bg);
+void FillCutVars(LowNuHighNuEvent&, const std::vector<Variable*>&);
+void FillStackedHists(const LowNuHighNuEvent&,
+                      const std::vector<Variable*>&);  // all variables
+void FillStackedHists(const LowNuHighNuEvent&, Variable*,
+                      const double fill_value = -999.);  // Single variable
+}  // namespace lownuhighnu_event
+
+
+#endif  // LowNuHighNuEvent
