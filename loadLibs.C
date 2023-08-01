470c58c82b6a30cffd05a81c4215471a695ad204
hyperfiner <rfine4@gmail.com>
2023-08-01 17:25:11 -0500

    Adding LowNuHighNuEvent.cxx to loadLibs.C

--- loadLibs.C
+++ loadLibs.C
@@ -24,6 +24,7 @@ void loadIncludes(bool verbose_cvu) {
   gSystem->CompileMacro("HadronVariable.cxx", "k");
   gSystem->CompileMacro("MacroUtil.cxx", "k");
   gSystem->CompileMacro("CCPiEvent.cxx", cvu_flags);
+  gSystem->CompileMacro("LowNuHighNuEvent.cxx", cvu_flags);
   gSystem->CompileMacro("WSidebandFitter.cxx", "k");
   gSystem->CompileMacro("CohDiffractiveSystematics.cxx", "k");
 }
