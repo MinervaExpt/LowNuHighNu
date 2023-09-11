8b0814aba238e69fa607425e3694edffa66f9e54
hyperfiner <rfine4@gmail.com>
2023-09-11 18:06:07 -0500

    Making setup script user-agnostic, though it will still be making some assumptions about how the user has their working area structured.
    Conflicts:
    	setup_mat.sh

--- setup_mat.sh
+++ setup_mat.sh
@@ -13,14 +13,14 @@ setup cmake v3_7_1
 
 source /cvmfs/minerva.opensciencegrid.org/minerva/setup/setup_minerva_products.sh
 
-export TOPDIR=/minerva/app/users/norrick/MATAna/
-CMTUSER=/minerva/app/users/norrick/cmtuser
-W=/minerva/app/users/norrick/MATAna/LowNuHighNu
-d=/pnfs/minerva/scratch/users/norrick
-D=/pnfs/minerva/persistent/users/norrick/LowNuHighNu/2023-06-05
-b=/minerva/data/users/norrick/
-m=/minerva/app/users/norrick/MATAna/MAT/
-mm=/minerva/app/users/norrick/MATAna/MAT-MINERvA/
+export TOPDIR=/minerva/app/users/$USER/MATAna/
+CMTUSER=/minerva/app/users/$USER/cmtuser
+W=/minerva/app/users/$USER/MATAna/LowNuHighNu
+d=/pnfs/minerva/scratch/users/$USER
+D=/pnfs/minerva/persistent/users/$USER/LowNuHighNu/2023-06-05
+b=/minerva/data/users/$USER/
+m=/minerva/app/users/$USER/MATAna/MAT/
+mm=/minerva/app/users/$USER/MATAna/MAT-MINERvA/
 
 cd $W
 
