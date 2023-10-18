d11e34797793759a161675155fc7f08cc4281b53
hyperfiner <rfine4@gmail.com>
2023-10-17 20:24:45 -0500

    Modify list of analysis variables for LowNuHighNu use case

--- xsec/makeCrossSectionMCInputs.C
+++ xsec/makeCrossSectionMCInputs.C
@@ -43,14 +43,8 @@ std::vector<Variable*> GetLowNuHighNuVariables(bool include_truth_vars = true) {
   Var* enu = new Var("enu", "E_{#nu}", "MeV", CCPi::GetBinning("enu"),
                      &CVUniverse::GetEnu);
 
-  Var* q2 = new Var("q2", "Q^{2}", "MeV^{2}", CCPi::GetBinning("q2"),
-                    &CVUniverse::GetQ2);
-
-  Var* ptmu = new Var("ptmu", "p^{T}_{#mu}", "MeV", CCPi::GetBinning("ptmu"),
-                      &CVUniverse::GetPTmu);
-
-  Var* pzmu = new Var("pzmu", "p^{z}_{#mu}", "MeV", CCPi::GetBinning("pzmu"),
-                      &CVUniverse::GetPZmu);
+  Var* ehad = new Var("ehad", "ehad", "MeV", CCPi::GetBinning("ehad"),
+                      &CVUniverse::GetEhad);
 
   // True Variables
   bool is_true = true;
@@ -67,35 +61,16 @@ std::vector<Variable*> GetLowNuHighNuVariables(bool include_truth_vars = true) {
       new Var("enu_true", "E_{#nu} True", enu->m_units,
               enu->m_hists.m_bins_array, &CVUniverse::GetEnuTrue, is_true);
 
-  Var* q2_true =
-      new Var("q2_true", "Q^{2} True", q2->m_units, q2->m_hists.m_bins_array,
-              &CVUniverse::GetQ2True, is_true);
-
-  Var* ptmu_true =
-      new Var("ptmu_true", "pt_{#mu} True", "MeV", ptmu->m_hists.m_bins_array,
-              &CVUniverse::GetPTmuTrue, is_true);
-
-  Var* pzmu_true =
-      new Var("pzmu_true", "pz_{#mu} True", "MeV", pzmu->m_hists.m_bins_array,
-              &CVUniverse::GetPZmuTrue, is_true);
-
-  // Ehad variables
-  Var* ehad = new Var("ehad", "ehad", "MeV", CCPi::GetBinning("ehad"),
-                      &CVUniverse::GetEhad);
   Var* ehad_true =
       new Var("ehad_true", "ehad True", "MeV", ehad->m_hists.m_bins_array,
-              &CVUniverse::GetEhadTrue);
-  ehad_true->m_is_true = true;
+              &CVUniverse::GetEhadTrue, is_true);
 
-  std::vector<Var*> variables = {pmu, thetamu_deg, enu, q2, ptmu, pzmu, ehad};
+  std::vector<Var*> variables = {pmu, thetamu_deg, enu, ehad};
 
   if (include_truth_vars) {
     variables.push_back(pmu_true);
     variables.push_back(thetamu_deg_true);
     variables.push_back(enu_true);
-    variables.push_back(q2_true);
-    variables.push_back(ptmu_true);
-    variables.push_back(pzmu_true);
     variables.push_back(ehad_true);
   }
 
