59e52e7ffedd96858fd02f634da8fae54713352e
hyperfiner <rfine4@gmail.com>
2024-06-27 14:47:15 -0500

    Clean up python binning file, deprecating old clutter and being explicit about MeV vs GeV for the flux hists

--- py_classes/binning.py
+++ py_classes/binning.py
@@ -2,27 +2,30 @@
 
 #### Neutrino Energy Binning
 #######################################
-#bins_nuE_LE = [0,1,2,3,4,5,7,9,12,15,18,22,36,50,75,100,120]
-#bins_nuE_LE = [0,1,2,3,4,5,6,7,8,9,10.5,12,13.5,15,18,22,36,50,75,100,120]
-#bins_nuE_LE = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,36,50,75,100,120]
-bins_nuE_LE = [0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,22,28,34,40,50,60,80,100,120]
-nBins_nuE_LE = len(bins_nuE_LE)-1
+bins_nuE_MeV = [0.,1.e3,2.e3,3.e3,4.e3,5.e3,6.e3,7.e3,8.e3,9.e3,1.e4,1.2e4,1.4e4,1.6e4,
+                1.8e4,2.0e4,2.2e4,2.8e4,3.4e4,4.0e4,5.0e4,6.0e4,8.0e4,1.0e5,1.2e5]
+bins_nuE_GeV = [0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,22,28,34,40,50,60,80,100,120]
+
+bins_nuE = bins_nuE_GeV
+nBins_nuE = len(bins_nuE)-1
+
+bins_nuE_LE = bins_nuE
+nBins_nuE_LE = nBins_nuE
 
 ## All signal definitions in LE get the default LE binning
 for sigDef in ["lowNu","highNu","inclusive"]:
-  exec("bins_nuE_{0}_LE = bins_nuE_LE".format(sigDef))
-  exec("nBins_nuE_{0}_LE = nBins_nuE_LE".format(sigDef))
+  exec("bins_nuE_{0}_LE = bins_nuE".format(sigDef))
+  exec("nBins_nuE_{0}_LE = nBins_nuE".format(sigDef))
 
 ## The ME high-nu analysis uses the LE binning
-bins_nuE_highNu_ME = bins_nuE_LE
-nBins_nuE_highNu_ME = nBins_nuE_LE
+bins_nuE_highNu_ME = bins_nuE
+nBins_nuE_highNu_ME = nBins_nuE
 
-## Signal definitions in ME used for the ME low-nu analysis get different binning
-#bins_nuE_ME = [0,1,2,3,4,5,6,7,8,9,10.5,12,13.5,15,18,22,36,50,75,100,120]
-bins_nuE_ME = [0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,22,28,34,40,50,60,80,100,120]
-binCenters_nuE_ME = [0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,11,13,15,17,19,21,25,31,37,45,55,70,90,110]
-#bins_nuE_ME = [0,1.5,3,4.5,6,7.5,9,10.5,12,13.5,15,20,25,30,50,75,100,120] # Amit's binning
-nBins_nuE_ME = len(bins_nuE_ME)-1
+## Signal definitions in ME used for the ME low-nu analysis may utilize different binning
+#bins_nuE_ME = [0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,22,28,34,40,50,60,80,100,120]
+#nBins_nuE_ME = len(bins_nuE_ME)-1
+bins_nuE_ME = bins_nuE
+nBins_nuE_ME = nBins_nuE
 
 for sigDef in ["lowNu","inclusive"]:
   exec("bins_nuE_{0}_ME = bins_nuE_ME".format(sigDef))
